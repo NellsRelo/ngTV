cd494531e24eb94871f55e2785fe9818b5ce6f7b
NellsRelo <kylehenglish@gmail.com>
2023-05-30 00:15:17 -0700

    Update NavBar
    - Utilize Material Tabs
    - Define tabs for Channels, Library, Settings, and Version
    
    Signed-off-by: NellsRelo <kylehenglish@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ngTV';
+  navLinks: any[];
+  activeLinkIndex = -1;
+
+  constructor(private router: Router) {
+    this.navLinks = [
+      {
+        label: 'Channels',
+        link: './channels',
+        index: 0
+      },
+      {
+        label: 'Library',
+        link: '',
+        index: 1
+      },
+      {
+        label: 'Settings',
+        link: '',
+        index: 2
+      },
+      {
+        label: 'Version',
+        link: '',
+        index: 3
+      },
+    ]
+  }
+
+  ngOnInit(): void {
+    this.router.events.subscribe((res) => {
+      this.activeLinkIndex = this.navLinks.indexOf(this.navLinks.find(tab => tab.link === '.' + this.router.url));
+    });
+  }
 }
